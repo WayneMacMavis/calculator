1cf448195d1a7ead4af48c3ba6c5bdb78553ccb4
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-07-19 10:05:58 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let oneBtn = document.getElementById('calc-one');
+let twoBtn = document.getElementById('calc-two');
+let threeBtn = document.getElementById('calc-three');
+let fourBtn = document.getElementById('calc-four');
+let fiveBtn = document.getElementById('calc-five');
+let sixBtn = document.getElementById('calc-six');
+let sevenBtn = document.getElementById('calc-seven');
+let eightBtn = document.getElementById('calc-eight');
+let nineBtn = document.getElementById('calc-nine');
+let zeroBtn = document.getElementById('calc-zero');
+
+let desimalBtn = document.getElementById('calc-desimal');
+let clearBtn = document.getElementById('calc-clear');
+let backspaceBtn = document.getElementById('calc-backspace');
+
+let displayElement = document.getElementById('calc-display-val');
+
+let calcNumBtns = document.getElementsByClassName('calc-btn-num');
+let calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
+
+for() {
+
+};
+for() {
+
+};
