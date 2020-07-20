6c416f254153a202f673011ea8bb1fb888a52f92
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-07-20 19:14:33 +0200

    complete calculator

--- script.js
+++ script.js
@@ -15,12 +15,99 @@ let backspaceBtn = document.getElementById('calc-backspace');
 
 let displayElement = document.getElementById('calc-display-val');
 
+let displayVal = '0';
+let pendingVal;
+let evalStringArray = [];
+
 let calcNumBtns = document.getElementsByClassName('calc-btn-num');
 let calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
 
-for() {
+let updateDisplayVal = (clickObj) => {
+let btnText = clickObj.target.innerText;
+
+if (displayVal === '0')
+    displayVal = '';
+
+    displayVal += btnText;
+    displayElement.innerText = displayVal;
+}
+
+let performOperation = (clickObj) => {
+let operator = clickObj.target.innerText;
+switch (operator) {
+    case '+':
+        pendingVal = displayVal;
+        displayVal = '0';
+        displayElement.innerText = displayVal;
+        evalStringArray.push(pendingVal);
+        evalStringArray.push('+');
+        break;
+
+    case '-':
+        pendingVal = displayVal;
+        displayVal = '0';
+        displayElement.innerText = displayVal;
+        evalStringArray.push(pendingVal);
+        evalStringArray.push('-');
+        break;
+
+        case 'x':
+        pendingVal = displayVal;
+        displayVal = '0';
+        displayElement.innerText = displayVal;
+        evalStringArray.push(pendingVal);
+        evalStringArray.push('*');
+        
+        break;
+
+        case '÷':
+        pendingVal = displayVal;
+        displayVal = '0';
+        displayElement.innerText = displayVal;
+        evalStringArray.push(pendingVal);
+        evalStringArray.push('/');
+        
+        break;
+
+        case '=':
+        evalStringArray.push(displayVal);
+        let evaluation = eval(evalStringArray.join(' '));
+        displayVal = evaluation + '';
+        displayElement.innerText = displayVal;
+        evalStringArray = [];
+        break;
+
+    default:
+        break;
+}
+}
+
+for(let i=0; i<calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+};
+for(let i=0; i<calcOperatorBtns.length; i++) {
+    calcOperatorBtns[i].addEventListener('click', performOperation, false);
 
 };
-for() {
 
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    displayElement.innerHTML = displayVal;
+}
+
+backspaceBtn.onclick = () => {
+    let lenghOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice('0', lenghOfDisplayVal - 1);
+
+    if(displayVal === '')
+    displayVal = '0';
+    displayElement.innerHTML = displayVal;
+}
+
+desimalBtn.onclick = () => {
+    if(!displayVal.includes('.'))
+    displayVal += '.';
+    displayElement.innerHTML = displayVal;
 };
